dbb2300c4e05d5925e142ad345d756221358b287
tinysuperion <tinysuperion@gmail.com>
2025-09-04 19:31:18 -0400

    added two-opt and nearest neighbor

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect} from 'react'
 
 let points = [];
 
-let delay = 50;
+let delayTime = 100;
 let pointCount = 5;
 let pointSize = 8;
 
@@ -10,7 +10,7 @@ let ongoing = false;
 let pause = false;
 let skip = false;
 
-let paths = [];
+let path = [];
 
 function App() {
 
@@ -43,15 +43,26 @@ function App() {
     // clear canvas
     context.clearRect(0,0, canvas.width, canvas.height);
     
-    paths.forEach((path)=>{
+    // paths.forEach((path)=>{
+
+    //   context.strokeStyle = "rgb(89, 158, 255)";
+    //   context.lineWidth = 5;
+    //   context.beginPath();
+    //   context.moveTo(path[0][0], path[0][1]);
+    //   context.lineTo(path[1][0], path[1][1]);
+    //   context.stroke();
+    // })
+
+    for (let index = 0; index < path.length-1; index++){
 
       context.strokeStyle = "rgb(89, 158, 255)";
       context.lineWidth = 5;
       context.beginPath();
-      context.moveTo(path[0][0], path[0][1]);
-      context.lineTo(path[1][0], path[1][1]);
-      context.stroke();
-    })
+      context.moveTo(path[index][0], path[index][1]);
+      context.lineTo(path[index+1][0], path[index+1][1]);
+      context.stroke(); 
+
+    }
 
     points.forEach((point)=>{
 
@@ -64,12 +75,29 @@ function App() {
 
   }
   
-  async function convexHull(){
+  function pathCost(point1, point2){
 
-    function pathCost(point1, point2){
+    if (point1 == undefined && point2 == undefined){
 
-      return Math.hypot(Math.abs(point1[0] - point2[0]), Math.abs(point1[1] - point2[1]));
+      let cost = 0;
+
+      for (let index = 0; index < path.length-2; index++){
+
+        // cost += pathCost(path[index], path[index+1]); // im sure this takes up more memory but its a lot cleaner
+
+        cost += Math.hypot(Math.abs(path[index][0] - path[index+1][0]), Math.abs(path[index][1] - path[index+1][1]));
+
+      }
+
+      return cost;
     }
+
+    return Math.hypot(Math.abs(point1[0] - point2[0]), Math.abs(point1[1] - point2[1]));
+  }
+
+
+
+  async function convexHull(){
 
     function mergeSort(array){
 
@@ -163,7 +191,7 @@ function App() {
       sorted[index].push(slope);
     }
 
-    // exclude the origin in the points since it doesnt have an associated slope
+    // exclude the origin in the points since it doesnt have an associated slope, also its the starting point
 
     sorted = sorted.slice(0,origin).concat(sorted.slice(origin+1));
 
@@ -174,6 +202,7 @@ function App() {
     // now loop through and use cross product to create convex hull
 
     let current = [points[origin]];
+    path.push(points[origin]);
 
     let excluded = [];
 
@@ -192,7 +221,7 @@ function App() {
 
         let result = (y2 * x1) - (y1 * x2);
 
-        if (result >= 0){
+        // if (result >= 0){
 
         while (result >= 0){
 
@@ -200,7 +229,7 @@ function App() {
 
           excluded.push(current[current.length-1]);
           current.pop();
-          paths.pop();
+          path.pop();
 
           x1 = current[current.length-1][0] - current[current.length-2][0];
           y1 = current[current.length-1][1] - current[current.length-2][1];
@@ -208,58 +237,76 @@ function App() {
           y2 = sorted[index][1] - current[current.length-2][1];
 
           result = (y2 * x1) - (y1 * x2);
-          await delay(100);
+          await delay(delayTime
+);
 
         }
 
-        current.push(sorted[index]);
-        paths.push([current[current.length-2],sorted[index]]);
-        continue;
+        // path.push(sorted[index]);
+        // current.push(sorted[index]);
+        // path.push([current[current.length-2],sorted[index]]);
+        // continue;
 
-        }
+        // }
       }
 
       // tack onto convex hull
 
-      paths.push([current[current.length-1], sorted[index]]);
+      // paths.push([current[current.length-1], sorted[index]]);
+      path.push(sorted[index]);
       current.push(sorted[index]);
 
       console.log(current);
 
       refresh();
-      await delay(100);
+      await delay(delayTime);
     }
 
-    paths.push([current[current.length-1], current[0]]);
+    // paths.push([current[current.length-1], current[0]]);
+    path.push(current[0]); // connect to origin
 
-    console.log(paths);
+    // console.log(paths);
+    console.log(path.slice());
 
     refresh();
 
     // connect remaining points to the convex hull
 
-    console.log(excluded, paths);
+    // console.log(excluded, paths);
+    console.log(excluded, path);
     
     while (excluded.length > 0){
 
-      let [bestCost, bestPointIndex, bestPathIndex] = [Infinity, null, null];
+      let [bestCost, bestPointIndex, insertIndex] = [Infinity, null, null];
 
       for (let [pointIndex, point] of excluded.entries()){
 
-        for (let [pathIndex, path] of paths.entries()){
-          
-          // compare distance from the points of the path to the new point then subtracts the length of the path
-          // in the first place to check for less loss
+        for (let index = 0; index < path.length-1; index++){
 
-          const cost = (pathCost(path[0], point) + pathCost(point, path[1])) - pathCost(path[0], path[1]);
+          const cost = (pathCost(path[index], point) + pathCost(point, path[index+1])) - pathCost(path[index], path[index+1]);          
 
           if (cost < bestCost){
 
-            [bestCost, bestPointIndex, bestPathIndex] = [cost, pointIndex, pathIndex];
+            [bestCost, bestPointIndex, insertIndex] = [cost, pointIndex, index];
 
           }
-          
+
         }
+
+        // for (let [pathIndex, path] of paths.entries()){
+          
+        //   // compare distance from the points of the path to the new point then subtracts the length of the path
+        //   // in the first place to check for less loss
+
+        //   const cost = (pathCost(path[0], point) + pathCost(point, path[1])) - pathCost(path[0], path[1]);
+
+        //   if (cost < bestCost){
+
+        //     [bestCost, bestPointIndex, bestPathIndex] = [cost, pointIndex, pathIndex];
+
+        //   }
+          
+        // }
 
       }
 
@@ -267,19 +314,21 @@ function App() {
 
       const [point] = excluded.splice(bestPointIndex, 1); // in brackets to unpack it out of the array splice returns deleted elements in
 
-      const path = paths[bestPathIndex].slice();
+      path.splice(insertIndex+1, 0, point);
 
-      paths.splice(bestPathIndex, 1);
+      // const path = paths[bestPathIndex].slice();
 
-      paths.push([path[0], point]);
-      paths.push([point, path[1]]);
+      // paths.splice(bestPathIndex, 1);
+
+      // paths.push([path[0], point]);
+      // paths.push([point, path[1]]);
 
       refresh();
-      await delay(100);
+      await delay(delayTime);
 
     }
 
-    console.log(paths);
+    console.log(path);
     // refresh();
 
     // return new Promise(()=>{
@@ -288,6 +337,101 @@ function App() {
 
 
     // })
+
+  }
+
+  async function twoOpt(){
+
+    if (path.length == 0){
+
+      // generate graph using nearest neighbor if there isnt a path to improve
+
+    }
+
+    let improvement = true;
+
+    let bestCost = pathCost(null, null);
+    console.log(bestCost);
+
+    while (improvement){
+
+      improvement = false;
+
+      for (let point1 = 1; point1 < path.length-1; point1++){
+        // reason for the 1 and the -1 for the 2nd point is due to the fact it always has to go back to the origin
+        // and if you start reversing a section with the origin in it suddenly 2 edges are going to the origin
+        // and the entire graph explodes, in other words it has to start with the origin and end with the origin
+        
+        for (let point2 = point1+1; point2 < path.length-1; point2++){
+
+          const section = path.slice(point1, point2+1); // gets the entire section between these 2 points to attempt to "untwist" it
+          section.reverse();
+
+          path.splice(point1, point2+1 - point1, ...section);
+
+          const cost = pathCost(null, null);
+
+          if (cost < bestCost){
+
+            improvement = true;
+            bestCost = cost;
+          }
+
+          else{
+
+            section.reverse();
+            path.splice(point1, point2+1 - point1, ...section);
+
+          }
+
+          refresh();
+
+        }
+
+        await delay(delayTime);
+
+      }
+
+    }
+
+  }
+
+  async function nearestNeighbor(){
+
+    let origin = points[0];
+    let pointsLeft = points.slice(1);
+
+    path.push(origin);
+
+    while (pointsLeft.length > 0){
+
+      let distance = 99999;
+      let closest;
+
+      for (let index = 0; index < pointsLeft.length; index++){
+
+        let difference = pathCost(origin, pointsLeft[index]);
+
+        if (difference < distance){
+
+          distance = difference;
+          closest = index;
+
+        }
+
+      }
+
+      path.push(pointsLeft[closest]);
+      origin = pointsLeft[closest];
+      pointsLeft.splice(closest, 1);
+      refresh();
+
+      await delay(delayTime);
+
+    }
+
+    path.push(path[0]);
+    refresh();
 
   }
 
@@ -319,6 +463,8 @@ function App() {
               <select id="algorithmSelection">
 
                 <option className="option" value="convex">convex hull</option>
+                <option className="option" value="2opt">2-opt</option>
+                <option className="option" value="nearest">nearest neighbor</option>
 
               </select>
 
@@ -339,9 +485,6 @@ function App() {
                   const formattedContent = changeIndent(fileContent, 2);
 
                   setCode(
-
-                    // <p id="code" style={{"opacity" : visibility}}><span style={{"fontWeight" : 500, 'fontSize' : 18}}>depth-first search implementation</span> <br/> copy and paste code</p>
-                    // isnt updated when done like this for some reason
 
                   <div>
                     <p style={{"fontSize": 14}}><span style={{"fontWeight" : 500, 'fontSize' : 18}}>depth-first search implementation<br/>
@@ -364,6 +507,11 @@ function App() {
             <div className="controls">
 
               <button className="control" id="run" onClick={async ()=>{
+
+                if (points.length == 0){
+
+                  return;
+                }
 
                 const run = document.getElementById("run");
                 run.textContent = "pause";
@@ -399,6 +547,16 @@ function App() {
 
                 }
 
+                else if (selection == "2opt"){
+
+                  twoOpt();
+                }
+
+                else if (selection == "nearest"){
+
+                  nearestNeighbor();
+                }
+
                 ongoing = false;
                 skip = false;
 
@@ -416,28 +574,6 @@ function App() {
 
               <button className="control" onClick={async ()=>{
 
-                let newGrid = grid;
-
-                if (ongoing){
-
-                  reset = true;
-                  await delay(Math.max(delayTime*2, 50));
-                }
-
-                for (let row = 0; row < newGrid.length; row++){
-
-                  for (let col = 0; col < newGrid.length; col++){
-
-                    if ((row != start[0] || col != start[1]) && (row != end[0] || col != end[1])){
-
-                      tiles[row * newGrid.length + col].current.textContent = "";
-                    }
-
-                    newGrid[row][col] = 0;
-                  }
-                }
-
-                setState(newGrid.slice());
               }}>
                 reset
               </button>
@@ -453,15 +589,13 @@ function App() {
             <input className="slider" id="delaySlider" type="range" min="0" max="500" step="10" defaultValue="50" onInput={async ()=>{
 
               const slider = document.getElementById("delaySlider");
-              delay = slider.value;
+              delayTime = slider.value;
 
               const size = slider.offsetWidth;
 
               const value = document.getElementById("value");
-              value.textContent = delay
-              setDelayPos(25 + size * (delay / 550));
-
-              console.log("size", size);
+              value.textContent = delayTime
+              setDelayPos(25 + size * (delayTime / 550));
 
             }}/>
 
@@ -490,13 +624,13 @@ function App() {
                 const height = canvas.offsetHeight;
                 for (let index = 0; index < pointCount; index++){
 
-                  const x = Math.floor(Math.random() *  width);
-                  const y = Math.floor(Math.random() *  height);
+                  const x = Math.min(Math.max(pointSize * 2.5, Math.floor(Math.random() *  width)),  width - pointSize * 2.5);
+                  const y = Math.min(Math.max(pointSize * 2.5, Math.floor(Math.random() *  height)),  height - pointSize * 2.5);
 
                   // const x = 100;
                   // const y = 10;
 
-                  console.log(x, y);
+                  // console.log(x, y);
 
                   // context.fillStyle = "white"
                   // context.beginPath();
@@ -545,15 +679,15 @@ function App() {
 
                 const size = slider.offsetWidth;
 
-                const value = document.getElementById("value");
+                const value = document.getElementById("sizeValue");
                 value.textContent = pointSize;
-                setSizePos(25 + size * (delay / 550));
+                setSizePos(25 + size * (pointSize / 550));
 
                 console.log("size", pointSize);
 
               }}/>
 
-              <p id="sizeValue" style={{"position" : "absolute", "height" : `${15}px`,"fontSize" : `${14}px`, "left" :  countPosition}}>50</p>
+              <p id="sizeValue" style={{"position" : "absolute", "height" : `${15}px`,"fontSize" : `${14}px`, "left" :  sizePosition}}>50</p>
 
             </div>
 
